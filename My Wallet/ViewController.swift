c718a971305131d1fa881efe8f28636ee898098b
MohamedDer <medder12@gmail.com>
2018-09-21 10:40:27 +0100

    Initial Commit

---
+++ My Wallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Wallet
+//
+//  Created by Mohamed Derkaoui on 21/09/2018.
+//  Copyright © 2018 Mohamed Derkaoui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
