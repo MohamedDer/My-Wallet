fde5924d1eb429d3bf49dba505270cfecead08c7
MohamedDer <medder12@gmail.com>
2018-09-24 10:34:50 +0100

    implement login screen UI

--- My Wallet/AppDelegate.swift
+++ My Wallet/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import IQKeyboardManager
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        IQKeyboardManager.shared().isEnabled = true
         return true
     }
 
--- My Wallet/Login Singup/LoginViewController.swift
+++ My Wallet/Login Singup/LoginViewController.swift
@@ -7,21 +7,34 @@
 //
 
 import UIKit
+import IQKeyboardManager
 
 class LoginViewController: UIViewController {
+    
 
     @IBOutlet weak var alphaView: UIView!
+    @IBOutlet weak var bottomAlphaView: UIView!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+
         alphaView.layer.cornerRadius = 15.0
+        bottomAlphaView.clipsToBounds = true
+        bottomAlphaView.layer.cornerRadius = 15.0
+        bottomAlphaView.layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
         loginButton.layer.cornerRadius = 10.0
+        signUpButton.layer.cornerRadius = 10.0
 
 
     }
 
     @IBAction func didClickLogin(_ sender: Any) {
+        
     }
     
+    @IBAction func didClickForgotPassword(_ sender: Any) {
+    }
 }
 
